f2cd6fbdd93674348088bcb061c33cce29392ecf
nsalvador <nigelsalvador@gmail.com>
2019-11-21 06:40:41 -0500

    Initial commit

---
+++ src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-text-field
+      outlined
+      placeholder="Search..."
+      append-icon="mdi-magnify"
+      @keydown.enter="search"
+      v-model="show"
+    ></v-text-field>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      show: ""
+    };
+  },
+  methods: {
+    search() {
+      this.$emit("search", this.show);
+    }
+  }
+};
+</script>
