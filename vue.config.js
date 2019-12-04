6fde2ea1fadb1b7a6c4153d817fb609594d78104
nsalvador <nigelsalvador@gmail.com>
2019-12-04 18:55:39 -0500

    Removed publicPath property from object in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ module.exports = {
 	devServer: {
 		proxy: 'http://localhost:3000'
 	},
-	publicPath:
-		process.env.NODE_ENV === 'production' ? '/tv-calendar-app-client/' : '/',
 	outputDir: 'docs'
 };
