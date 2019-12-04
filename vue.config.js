3dfd631acd18b6939d4b1b80327a36fdeb920abf
nsalvador <nigelsalvador@gmail.com>
2019-12-04 14:26:54 -0500

    Updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = {
 		proxy: 'http://localhost:3000'
 	},
 	publicPath:
-		process.env.NODE_ENV === 'production' ? 'tv-calendar-app-client' : '/',
+		process.env.NODE_ENV === 'production' ? 'tv-calendar-app-client/' : '/',
 	outputDir: 'docs'
 };
